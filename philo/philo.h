2e0ae1588ce8a5f452f73ea0a975b7df06067efc
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-05-19 15:47:59 +0200

    init function added, ft_time  added

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 16:26:03 by vde-prad          #+#    #+#             */
-/*   Updated: 2023/05/17 16:29:22 by vde-prad         ###   ########.fr       */
+/*   Updated: 2023/05/19 13:27:35 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 
 /**************** DEFINITIONS *****************/
 # include <pthread.h>
+# include <stdio.h>
+# include <sys/time.h>
+
+typedef struct s_data{
+	struct timeval	time;
+	time_t			stime;
+}	t_data;
+
 /***************** FUNCTIONS ******************/
 
 #endif
