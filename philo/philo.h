2d5953c716f4d081b422da4ea1ffdbb500560519
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-05-26 19:54:08 +0200

    Threads created
    But the death of the philosphers must be controlled

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 16:26:03 by vde-prad          #+#    #+#             */
-/*   Updated: 2023/05/22 15:24:42 by vde-prad         ###   ########.fr       */
+/*   Updated: 2023/05/25 20:17:21 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,28 @@
 /**************** DEFINITIONS *****************/
 # include <pthread.h>
 # include <stdio.h>
+# include <unistd.h>
 # include <sys/time.h>
+# include <stdlib.h>
+
+typedef struct s_philo{
+	pthread_t		tid;
+	pthread_mutex_t	fork1;
+	pthread_mutex_t	*fork2;
+	pthread_mutex_t	finish_mutex;
+	time_t			last_eat;
+	time_t			stime;
+	int				index;
+	int				finish;
+	int				die_time;
+	int				eat_time;
+	int				sleep_time;
+	int				n_lunches;
+}	t_philo;
 
 typedef struct s_data{
-	struct timeval	time;
 	time_t			stime;
+	t_philo			*philos;
 	int				n_philo;
 	int				die_time;
 	int				eat_time;
@@ -31,8 +48,8 @@ typedef struct s_data{
 /***************** FUNCTIONS ******************/
 
 // init.c
-void	ft_init(t_data *data);
 int		ft_input(int argc, char **argv, t_data *data);
+int		ft_init_philosophers(t_data *data);
 // utils.c
 int		ft_atoi(const char *str);
 int		ft_check_args(char *arg);
