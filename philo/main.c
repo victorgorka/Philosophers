4e2c58da6e37e6850d1c925c17a2f1ada7ae6cc5
victor <vde-prad@student.42malaga.com>
2023-05-27 20:13:28 +0200

    It works in all cases but when there is a number of lunches it fails

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 16:39:54 by vde-prad          #+#    #+#             */
-/*   Updated: 2023/05/26 21:53:39 by vde-prad         ###   ########.fr       */
+/*   Updated: 2023/05/27 20:12:40 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
  * @param time struct where gettimeofday will write the info that it got
  * @return returns the number of miliseconds
 */
-int	ft_time(int option)
+time_t	ft_time(time_t option)
 {
 	struct timeval	time;
 	time_t			t;
@@ -27,7 +27,7 @@ int	ft_time(int option)
 	t = 0;
 	gettimeofday(&time, 0);
 	t = (time.tv_sec * 1000) + (time.tv_usec / 1000);
-	if (option < 0)
+	if (option == -1)
 		return (t);
 	else
 		return (t - option);
@@ -37,7 +37,10 @@ int	ft_inanition(t_philo *philos)
 {
 	pthread_mutex_lock(&philos->finish_mutex);
 	if (philos->finish)
+	{
+		pthread_mutex_unlock(&philos->finish_mutex);
 		return (1);
+	}
 	pthread_mutex_unlock(&philos->finish_mutex);
 	return (0);
 }
@@ -51,30 +54,51 @@ void	*rutina(void *data)
 	{
 		pthread_mutex_lock(&philos->fork1);
 		if (ft_inanition(philos))
+		{
+			pthread_mutex_unlock(&philos->fork1);
 			return (NULL);
-		printf("%d ms %d has taken fork 1\n", ft_time(philos->stime), philos->index + 1);
+		}
+		printf("%ld ms %d has taken fork 1\n", ft_time(philos->stime), philos->index + 1);
 		if (philos->fork2 != NULL)
 			pthread_mutex_lock(philos->fork2);
 		else
-			usleep(philos->die_time * 1000);
+		{
+			while (1)
+			{
+				if (ft_inanition(philos))
+				{
+					pthread_mutex_unlock(&philos->fork1);
+					return (NULL);
+				}
+			}
+		}
 		if (ft_inanition(philos))
+		{
+			pthread_mutex_unlock(&philos->fork1);
+			pthread_mutex_unlock(philos->fork2);
 			return (NULL);
-		printf("%d ms %d has taken fork 2\n", ft_time(philos->stime), philos->index + 1);
-		printf("%d ms %d is eating\n", ft_time(philos->stime),
+		}
+		printf("%ld ms %d has taken fork 2\n", ft_time(philos->stime), philos->index + 1);
+		printf("%ld ms %d is eating\n", ft_time(philos->stime),
 			philos->index + 1);
 		usleep(philos->eat_time * 1000);
 		if (ft_inanition(philos))
+		{
+			pthread_mutex_unlock(&philos->fork1);
+			pthread_mutex_unlock(philos->fork2);
 			return (NULL);
+		}
 		philos->last_eat = ft_time(-1);
 		pthread_mutex_unlock(&philos->fork1);
 		pthread_mutex_unlock(philos->fork2);
-		printf("%d ms %d is sleeping\n", ft_time(philos->stime), philos->index + 1);
+		printf("%ld ms %d is sleeping\n", ft_time(philos->stime), philos->index + 1);
 		usleep(philos->sleep_time * 1000);
 		if (ft_inanition(philos))
 			return (NULL);
-		printf("%d ms %d is thinking\n", ft_time(philos->stime), philos->index + 1);
+		printf("%ld ms %d is thinking\n", ft_time(philos->stime), philos->index + 1);
 	}
 	return (NULL);
+			return (NULL);
 }
 
 int	main(int argc, char **argv)
@@ -89,7 +113,7 @@ int	main(int argc, char **argv)
 	while (i < data.n_philo)
 	{
 		if (i % 2 != 0)
-			usleep(50);
+			usleep(100);
 		pthread_create(&data.philos[i].tid, NULL, rutina, &data.philos[i]);
 		i++;
 	}
@@ -98,16 +122,16 @@ int	main(int argc, char **argv)
 		i = 0;
 		while (i < data.n_philo)
 		{
-			if (ft_time(-1) - data.philos[i].last_eat > data.die_time)
+			if (ft_time(-1) - data.philos[i].last_eat >= data.die_time)
 			{
-				// printf("last eat off dead philo %ld\n", data.philos[i].last_eat);
-				printf("%d ms %d has died\n", ft_time(data.stime), i + 1);
+				printf("%ld ms %d has died\n", ft_time(data.stime), i + 1);
 				i = 0;
 				while (i < data.n_philo)
 				{
 					pthread_mutex_lock(&data.philos[i].finish_mutex);
-					data.philos[i++].finish = 1;
+					data.philos[i].finish = 1;
 					pthread_mutex_unlock(&data.philos[i].finish_mutex);
+					i++;
 				}
 				i = 0;
 				while (i < data.n_philo)
